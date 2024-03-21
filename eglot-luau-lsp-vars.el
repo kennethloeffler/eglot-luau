ce93ab8b3c7e51fc22118ea8c17afb150635a117
kennethloeffler <kenloef@gmail.com>
2024-03-21 14:08:28 -0700

    Add license disclaimer and packaging info

--- eglot-luau-lsp-vars.el
+++ eglot-luau-lsp-vars.el
@@ -1,5 +1,22 @@
 ;;; eglot-luau-lsp-vars.el --- variables for luau-lsp eglot client -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2024 Kenneth Loeffler
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This file contains variables and configuration for eglot-luau-lsp.
 
@@ -14,7 +31,7 @@
 ;; URL providing latest Roblox version
 (defconst eglot-luau-lsp-roblox-version-url "https://raw.githubusercontent.com/CloneTrooper1019/Roblox-Client-Tracker/roblox/version.txt")
 
-;; Declare eglot variables to silence warnings
+;; Forward declare eglot variables to silence warnings
 (defvar eglot-server-programs)
 
 ;; User customization
--- eglot-luau-lsp.el
+++ eglot-luau-lsp.el
@@ -1,5 +1,28 @@
 ;;; eglot-luau-lsp.el --- eglot client for luau-lsp -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2024 Kenneth Loeffler
+
+;; Author: Kenneth Loeffler <kenloef@gmail.com>
+;; Version: 0.1.0
+;; Keywords: roblox, luau, tools
+;; URL: https://github.com/kennethloeffler/eglot-luau-lsp
+;; Package-Requires: ((emacs "29.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This package provides a luau-lsp
 ;; (https://github.com/JohnnyMorganz/luau-lsp) integration for eglot.
